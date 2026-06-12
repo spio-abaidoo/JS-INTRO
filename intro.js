235305f57ef71cea3086cbc7b01661c7ae07e700
spioabaidoo <spioabaidoo@gmail.com>
2026-06-12 23:28:04 +0000

    First Commit

---
+++ intro.js
@@ -0,0 +1,10 @@
+const user ={
+    name: 'Ekow',
+    age: 29,
+    favoriteProgramingLanguge: 'JavaScript'
+
+};
+
+console.log(user.name);
+console.log(user.age);
+console.log(user.favoriteProgramingLanguge);
